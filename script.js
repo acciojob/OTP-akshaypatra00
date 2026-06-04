9179c61caa26c11ed9fa2d105279f73064d179d2
Akshay patra <akshaykumarpatra00@gmail.com>
2026-06-04 12:25:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+ const inputs = document.querySelectorAll('.code');
+        inputs.forEach((code)=>{
+     
+       code.addEventListener("keyup" , function(e){
+        if(e.key === "Backspace" && code.previousElementSibling){
+            code.previousElementSibling.focus()
+        }else if(code.nextElementSibling){
+            code.nextElementSibling.focus();
+        }
+
+       });
+       });
